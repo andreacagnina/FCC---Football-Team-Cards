71de0d225bf115d3555b26a49289b6d933fa70e5
andreacagnina <amirkal@libero.it>
2024-12-11 18:28:03 +0100

    [feat] step 37

--- script.js
+++ script.js
@@ -173,3 +173,28 @@ const myFavoriteFootballTeam = {
 
 Object.freeze(myFavoriteFootballTeam);
 const { sport, team, year, players } = myFavoriteFootballTeam;
+const { coachName } = myFavoriteFootballTeam.headCoach;
+
+typeOfSport.textContent = sport;
+teamName.textContent = team;
+worldCupYear.textContent = year;
+headCoach.textContent = coachName;
+
+const setPlayerCards = (arr = players) => {
+    playerCards.innerHTML += arr
+        .map(
+            ({ name, position, number, isCaptain, nickname }) => {
+                return `
+        <div class="player-card">
+          <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
+          <p>Position: ${position}</p>
+          <p>Number: ${number}</p>
+          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
+        </div>
+      ` }
+        )
+        .join("");
+};
+
+playersDropdownList.addEventListener("change", (e) => {
+    playerCards.innerHTML = "";
